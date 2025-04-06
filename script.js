854e6d7b510f6335152f7410e3d1d14955311f5a
Ruby Aguilar Becerra <aguibece21@gmail.com>
2025-04-05 23:27:54 -0500

    Primer commit - Manual del equipo de medios AVIVAAjuste

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
   toggleBtn.textContent = document.body.classList.contains('dark-mode') ? '☀️ Modo Claro' : '🌙 Modo Oscuro';
 });
+
+
+function toggleSidebar() {
+    document.querySelector('.sidebar').classList.toggle('open');
+  }
+  
